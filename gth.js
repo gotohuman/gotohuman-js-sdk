0bd16c5daaa943b0c074cc640cbb4bfa856cab01
Till Simon <mail@tillsimon.com>
2024-06-09 14:47:19 +0200

    Init

---
+++ gth.js
@@ -0,0 +1,87 @@
+const { v4: uuidv4 } = require('uuid');
+
+class GoToHuman {
+    constructor({apiKey, agentId, agentRunId = uuidv4(), fetch}) {
+        this.apiKey = apiKey;
+        this.agentId = agentId;
+        this.agentRunId = agentRunId;
+        this.fetch = fetch;
+    }
+
+    async callGoToHuman({state, params, stepResult = null, actionValues = null, allowEditing = false}) {
+        const url = "https://api.gotohuman.com/postRunState";
+        console.log("working with GTHURL: " + url);
+        const data = {
+            apiKey: this.apiKey,
+            agentId: this.agentId,
+            agentRunId: this.agentRunId,
+            state: state,
+            ...params,
+            ...(stepResult && { result: stepResult }),
+            ...(actionValues && { actionValues: actionValues, allowEditing: allowEditing }),
+            millis: Date.now()
+        };
+
+        const response = await this.fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log("Request successful! Response:", responseData);
+            return responseData;
+        } else {
+            const responseText = await response.text();
+            console.error(`Failed to make request. Status Code: ${response.status} Response: ${responseText}`);
+            throw new Error(`Failed to send to gotoHuman: ${responseText}`);
+        }
+    }
+
+    async startedTask({id, name = null, desc = null}) {
+        console.log(`startedTask ${id}`);
+        this.lastTaskId = id;
+        await this.callGoToHuman({state: "task_running", params: {
+            taskId: id,
+            ...(name && { taskName: name }),
+            ...(desc && { taskDesc: desc })
+        }});
+    }
+
+    async completedTask({id = null, name = null, desc = null, result = null}) {
+        const idToSend = id || this.lastTaskId;
+        console.log(`completedTask ${idToSend} ${result}`);
+        await this.callGoToHuman({state: "task_done", params: {
+            taskId: idToSend,
+            ...(name && { taskName: name }),
+            ...(desc && { taskDesc: desc }),
+        }, ...(!!result && { stepResult: result })});
+    }
+
+    async requestHumanApproval({taskId = null, taskName = null, taskDesc = null, actionValues = null, allowEditing = true, completedTasks = null}) {
+        console.log(`requestHumanApproval ${taskId} ${taskName} ${taskDesc} ${actionValues} ${allowEditing} ${completedTasks}`);
+        await this.callGoToHuman({state: "requested_human_approval", params:{
+            ...(taskId && { taskId: taskId }),
+            ...(taskName && { taskName: taskName }),
+            ...(taskDesc && { taskDesc: taskDesc }),
+            ...(completedTasks && { completedTasks: completedTasks })
+        },...(actionValues !== null && { actionValues: actionValues }), allowEditing: allowEditing});
+    }
+
+    async serveToHuman({taskId = null, taskName = null, taskDesc = null, actionValues = null, completedTasks = null}) {
+        console.log(`serveToHuman ${taskId}  ${taskName} ${taskDesc} ${actionValues} ${completedTasks}`);
+        await this.callGoToHuman({state: "served_to_human", params: {
+            ...(taskId && { taskId: taskId }),
+            ...(taskName !== null ? { taskName: taskName } : {}),
+            ...(taskDesc !== null ? { taskDesc: taskDesc } : {}),
+            ...(completedTasks !== null ? { completedTasks: completedTasks } : {})
+        },...(actionValues !== null ? { actionValues: actionValues } : {})});
+    }
+
+    async archive() {
+      await this.callGoToHuman({state: "archived"})
+    }
+}
+
+module.exports = GoToHuman;
---
+++ index.js
@@ -0,0 +1,71 @@
+import GoToHuman from "./gth";
+
+class AiFlow {
+  constructor({
+    onTrigger,
+    agentId,
+    fetch
+  }) {
+    this.steps = [];
+    this.onTrigger = onTrigger;
+    this.agentId = agentId;
+    this.fetch = fetch;
+  }
+
+  step({id, name = null, options = {}, fn}) {
+    if (id == undefined || fn == undefined)
+      throw new Error('id and fn are required');
+    if (this.steps.findIndex((val) => val.id == id) >= 0)
+      throw new Error('task id needs to be unique');
+    const stepFn = async(args) => {
+      await this.gth.startedTask({id: id, name: name})
+      const result = await fn(args);
+      await this.gth.completedTask({id: id, name: name, result: result})
+      return result;
+    }
+    this.steps.push({ id, name, stepFn });
+  }
+
+  gotoHuman({id, name = null, options = {}}) {
+    const stepFn = async({input}) => {
+      console.log(`gotoHuman for input ${typeof input}: ${input}`);
+      const actionValuesToSend = Array.isArray(input) ? input : [input];
+      console.log(`gotoHuman send actionValues: ${JSON.stringify(actionValuesToSend)}`);
+      if (!!options && options.onlyServeResults) {
+        await this.gth.serveToHuman({taskId: id, taskName: name, actionValues: actionValuesToSend})
+      } else {
+        await this.gth.requestHumanApproval({taskId: id, taskName: name, actionValues: actionValuesToSend, ...(!!options && !!options.allowEditing && {allowEditing: options.allowEditing})})
+      }
+      return null;
+    }
+    this.steps.push({ id, name, stepFn, interrupt: true });
+  }
+
+  async executeSteps({apiKey, agentId, triggerEvent, runId, actionValues, taskId, humanResponse}) {
+    if (!!agentId && (agentId !== this.agentId)) return null; //agentId explicitely targeted, but its not this agent
+    if (!!triggerEvent && (triggerEvent !== this.onTrigger)) return null; // there is a triggerEvent but its not for this agent
+    try {
+      this.gth = new GoToHuman({apiKey: apiKey, agentId: this.agentId, ...(runId && {agentRunId: runId}), fetch: this.fetch })
+      console.log("start flow")
+      const comingFromUserDecision = !!humanResponse;
+      if (!comingFromUserDecision && this.onTrigger != undefined)
+        await this.gth.completedTask({id: this.onTrigger, result: JSON.stringify(actionValues)})
+      let stepInput = actionValues;
+      console.log(`comingFromUserDecision ${comingFromUserDecision} taskId ${taskId} taskIndex ${this.steps.findIndex(val => val.id === taskId)}`)
+      const firstStepToRun = comingFromUserDecision ? (this.steps.findIndex(val => val.id === taskId) + 1) : 0;
+      console.log(`firstStepToRun ${firstStepToRun} of entries ${this.steps}`)
+      const stepsQueued = this.steps.slice(firstStepToRun);
+      for (const [index, step] of stepsQueued.entries()) {
+        console.log(`run step ${index} ${step.id}`)
+        stepInput = await step.stepFn({ flow: this, input: stepInput, infos: {approved: (!!humanResponse && (humanResponse == 'human_approved'))} });
+        if (step.interrupt) break;
+        if (index == (stepsQueued.length - 1))
+          await this.gth.archive();
+      }
+      return {success: true, ...(stepInput && {lastResult: stepInput})};
+    } catch (err) {
+      return {error: true, errorMsg: err.message}
+    }
+  }
+}
+export default AiFlow;
