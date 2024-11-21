0b799555d42abc3f39ecfd1e66f534236cfea5be
Till Simon <mail@tillsimon.com>
2024-11-21 15:13:23 +0100

    0.2.6 assignTo

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,8 @@ export type ReviewResponse = {
 export class Review {
   private fields: FormFields = {};
   private meta: FormFields = {};
+  private assignTo?: string[];
+  private assignToGroups?: string[];
 
   constructor(
     private readonly formId: string,
@@ -80,6 +82,24 @@ export class Review {
   }
 
   /**
+   * Assign the review request to specific users
+   */
+  assignToUsers(userEmails: string[]): Review {
+    if (userEmails.length > 0)
+      this.assignTo = userEmails;
+    return this;
+  }
+
+  /**
+   * Assign the review request to specific user groups
+   */
+  assignToUserGroups(groupIds: string[]): Review {
+    if (groupIds.length > 0)
+      this.assignToGroups = groupIds;
+    return this;
+  }
+
+  /**
    * Send the review request to the API
    */
   async sendRequest(): Promise<ReviewResponse> {
@@ -96,6 +116,8 @@ export class Review {
           formId: this.formId,
           fields: this.fields,
           meta: this.meta,
+          ...(this.assignTo && {assignTo: this.assignTo}),
+          ...(this.assignToGroups && {assignToGroups: this.assignToGroups}),
           millis: Date.now(),
           origin: "ts-sdk",
           originV: version,
@@ -116,18 +138,20 @@ export class Review {
 
 // Main SDK class
 export class GotoHuman {
-  private readonly baseUrl: string = 'https://api.gotohuman.com';
+  private baseUrl: string;
   private apiKey: string;
   private fetchImpl: FetchLike;
 
   constructor(
-    apiKey: string | undefined,
+    apiKey?: string,
     config: GotoHumanConfig = {}
   ) {
-    if (!apiKey) {
-      throw new Error('Please pass an API key');
+    this.baseUrl = GotoHuman.getBaseUrlFromEnv() || 'https://api.gotohuman.com';
+    const resolvedApiKey = apiKey || GotoHuman.getApiKeyFromEnv();
+    if (!resolvedApiKey) {
+      throw new Error('Please pass an API key either as a parameter or set the GOTOHUMAN_API_KEY environment variable.');
     }
-    this.apiKey = apiKey;
+    this.apiKey = resolvedApiKey;
     this.fetchImpl = config.fetch || globalThis.fetch;
   }
 
@@ -139,5 +163,29 @@ export class GotoHuman {
       throw new Error('Please pass a form ID');
     }
     return new Review(formId, this.apiKey, this.baseUrl, this.fetchImpl);
+  }
+
+  /**
+   * Retrieves the base URL from the environment variable.
+   * @returns The base URL if set, otherwise undefined.
+   */
+  private static getBaseUrlFromEnv(): string | undefined {
+    // Ensure this code runs only in Node.js environments
+    if (typeof process !== 'undefined' && process.env && process.env.GOTOHUMAN_BASE_URL) {
+      return process.env.GOTOHUMAN_BASE_URL;
+    }
+    return undefined;
+  }
+
+  /**
+   * Retrieves the API key from the environment variable.
+   * @returns The API key if set, otherwise undefined.
+   */
+  private static getApiKeyFromEnv(): string | undefined {
+    // Ensure this code runs only in Node.js environments
+    if (typeof process !== 'undefined' && process.env && process.env.GOTOHUMAN_API_KEY) {
+      return process.env.GOTOHUMAN_API_KEY;
+    }
+    return undefined;
   }
 }
